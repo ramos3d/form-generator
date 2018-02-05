6c5f4549790cd09185639e29d33470ee01bd6743
Marcelo Ramos Soares <marceloxramos3d@gmail.com>
2018-02-05 00:17:12 -0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
    	$host = "localhost";
 	$user = "root";     // Alter to your User
-	$pass= "ramos3d";  // Alter to your password
-	$db	  = "generator"; // Alter to your darabase name
+	$pass= "";  // Alter to your password
+	$db	  = ""; // Alter to your darabase name
 	$Mysqli = new mysqli($host, $user, $pass, $db);
 	//checking your connection
 	mysqli_connect_errno()? printf("Connect failed: %s\n", mysqli_connect_error()): "Connected";
