ff5b149d2f3fae93e6eeb5aa52aa837f77352b10
Ramos3d <marceloxramos3d@gmail.com>
2018-08-21 23:08:21 -0300

    header

--- index.php
+++ index.php
@@ -10,13 +10,7 @@
 <body>
     <div class="container">
         <div class="row">
-            <h1> <i class="fas fa-database "></i> Form Generator</h1>
-            <div class="alert alert-info">
-                <strong>First Steps!</strong> <p>Welcome to Form-Generator project,</p>
-                <p>Please, do not forget to configure the connection.php file with your connection datas before to continue.</p>
-                <p>Once done, fill out the form bellow with your both Table name and fold path. Then click on Generate Form. It's pretty simple and easy, </p>
-                <p>I hope you enjoy!</p>
-            </div>
+            <?php include "include/header.php";?>
             <form action="#" method="GET">
                 <div class="col-sm-12">
                     <div class="form-group col-xs-4">
@@ -25,7 +19,7 @@
                     </div>
                     <div class="form-group col-xs-4">
                         <label>Path:</label>
-                        <input type="text" class="form-control"  name="path" placeholder=/formCreated">
+                        <input type="text" class="form-control"  name="path" placeholder="/formCreated">
                         (Place where your form will be generated)
                     </div>
                 </div>
@@ -55,6 +49,6 @@
             }
             ?>
         </div>
-        <?=include"include/footer.php";?>
+        <?php include "include/footer.php"; ?>
     </body>
     </html>
